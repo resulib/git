6fbca26ba9cbf5b6a3e98adcc4ee17e95cd4a47d
resulib <mibadullayev98@gmail.com>
2024-04-22 04:55:59 +0400

    static block

--- Hello.java
+++ Hello.java
@@ -3,4 +3,8 @@ public class Hello{
     public static void main(String args []){
         System.out.println("Hello World.");
     }
+
+    static {
+        System.out.println("Static block in Hello Class");
+    }
 }
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+
+    static {
+        System.out.println("Static block");
+    }
+}
